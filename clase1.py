1c2bc41164473096f712638e297aa114cba1f440
JaacksAdriano <157839958+JaacksAdriano@users.noreply.github.com>
2025-07-10 11:09:06 -0600

    Add files via upload

---
+++ clase1.py
@@ -0,0 +1,14 @@
+#variable
+nombre = "Jaqueline"
+edad =17
+
+estatura= 1.54
+
+time_licencia =True
+
+#operador lógico de comparación 
+
+puede_conducir =edad >= 18 and time_licencia
+
+#Resultado
+print(nombre, "Puede conducir:", puede_conducir)
